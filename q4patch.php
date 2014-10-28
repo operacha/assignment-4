4a61b1f376d0bfd5bb24bb9cc1739bbd936c8336
Ozair Peracha <ozairmperacha@gmail.com>
2014-10-27 19:33:14 -0700

    making changes to patch

--- q4patch.php
+++ q4patch.php
@@ -1,13 +1,10 @@
 <?php
 
-	$out =  bin2hex(file_get_contents("30563118.program2.exe"));
-	//echo $out."\n";
-	//echo strpos ( $out , '7c' );
+	$hexfile =  bin2hex(file_get_contents("30563118.program2.exe"));
+
+	$temp = str_replace(strtolower("A1F0E599A522713841785ED1EED73710A50DDF96"),sha1($argv[1]),$hexfile);
+	$binary = hex2bin($temp);
 	
-	//echo strpos( $out, strtolower( 'A1F0E599A522713841785ED1EED73710A50DDF96' ) );
-	$new = str_replace(strtolower("A1F0E599A522713841785ED1EED73710A50DDF96"),sha1($argv[1]),$out);
-	$new_bin = hex2bin($new);
-	
-	file_put_contents ( 'tester.exe', $new_bin );
+	file_put_contents ( 'tester.exe', $binary );
 	
 ?>
